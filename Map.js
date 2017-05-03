e92ea33721a89262106309321c986ef7a250c3e0
Igor Knop <igorknop@gmail.com>
2017-05-02 21:40:45 -0300

    Adiciona mapa inicial

--- Map.js
+++ Map.js
@@ -13,21 +13,30 @@ Map.prototype.desenhar = function(ctx) {
   for (var i = 0; i < this.cells.length; i++) {
     var linha = this.cells[i];
     for (var j = 0; j < linha.length; j++) {
-      switch(this.cells[i][j]) {
+      switch (this.cells[i][j]) {
         case 0:
-        ctx.fillStyle = 'black';
-        ctx.fillRect(j * this.SIZE, i * this.SIZE, this.SIZE, this.SIZE);
-        break;
+          ctx.fillStyle = 'black';
+          ctx.fillRect(j * this.SIZE, i * this.SIZE, this.SIZE, this.SIZE);
+          break;
         case 1:
-        ctx.fillStyle = 'brown';
-        ctx.strokeStyle = 'tan';
-        ctx.fillRect(j * this.SIZE, i * this.SIZE, this.SIZE, this.SIZE);
-        ctx.lineWidth = 3;
-        ctx.strokeRect(j * this.SIZE, i * this.SIZE, this.SIZE, this.SIZE);
-        break;
-        default: ctx.fillStyle = 'red';
-        ctx.fillRect(j * this.SIZE, i * this.SIZE, this.SIZE, this.SIZE);
+          ctx.fillStyle = 'brown';
+          ctx.strokeStyle = 'tan';
+          ctx.fillRect(j * this.SIZE, i * this.SIZE, this.SIZE, this.SIZE);
+          ctx.lineWidth = 3;
+          ctx.strokeRect(j * this.SIZE, i * this.SIZE, this.SIZE, this.SIZE);
+          break;
+        default:
+          ctx.fillStyle = 'red';
+          ctx.fillRect(j * this.SIZE, i * this.SIZE, this.SIZE, this.SIZE);
       }
+    }
+  }
+};
+
+Map.prototype.loadMap = function(map) {
+  for (var i = 0; i < this.cells.length; i++) {
+    for (var j = 0; j < this.cells[i].length; j++) {
+      this.cells[i][j] = map[i][j];
     }
   }
 };
